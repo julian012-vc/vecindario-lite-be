2a78893c564fc837bb3719dbfe8887633d22695d
Julián Camilo Serna Vargas <julian.serna@vecindario.com>
2021-06-10 23:04:49 -0500

    :boom: MailChimp config and email body added (#9)

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address => ENV['SMTP_ADDRESS'],
+  :authentication => :plain,
+  :domain => ENV['SMTP_DOMAIN'],
+  :enable_starttls_auto => true,
+  :password => ENV['SMTP_API_KEY'],
+  :port => 587,
+  :user_name => ENV['SMTP_USER_NAME']
+}
